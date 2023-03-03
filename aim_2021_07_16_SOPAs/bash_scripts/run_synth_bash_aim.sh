6bc85bd6a5c5eeaa685cf7ceb8b132b5f0fcb04c
Howard Dao <howard.dao@gmail.com>
2023-03-02 23:49:03 -0500

    added a lot of runs, modified aim f_run_designspace

--- aim_2021_07_16_SOPAs/bash_scripts/run_synth_bash_aim.sh
+++ aim_2021_07_16_SOPAs/bash_scripts/run_synth_bash_aim.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -l
 
 # Runs synthesis pipeline
-# Inputs are lambda, optimal angle, discretization size, 'up' or 'down', geometry ('a', 'b', etc)
+# Inputs are lambda, optimal angle, discretization size, 'up' or 'down', geometry ('default', 'custom', etc), nitride-to-nitride spacing, top silicon nitride thickness
 # Specific for aim grating
 #
 # Example
-#	qsub run_synth_bash_aim.sh 1300 10 5 up 'a'
+#	qsub run_synth_bash_aim.sh 1300 10 5 up custom 50 160
 #		The above runs the synthesis for wavelength of 1300nm, angle 10 deg, and disc. size of 10nm
-#		for optimizing unidirectionality upwards
-#       design type 'a' (default)
+#		for optimizing unidirectionality upwards. 
+#               Custom layer stack with 50 nm nitride to nitride spacing, 160 nm thick top nitride gratings.
+#       design type 'default' for standard layer stack
 
 # set job name
 #$ -N grating_synth
@@ -17,7 +18,7 @@
 #$ -pe omp 28
 
 # specify hard time limit for job
-#$ -l h_rt=23:00:00
+#$ -l h_rt=16:00:00
 
 # merge output and error files in one
 #$ -j y
@@ -29,5 +30,4 @@
 module load matlab/2019a
 
 # run script
-# matlab -nodisplay -r "addpath('/project/siphot/bz/code/utility'); addpath('/projectnb/siphot/bz/git/grating_synthesis/chips/210716_AIM_SOPAs'); f_run_designspace_2level_aim( $1, $2, $3, '$4', '$5' ); exit"
-matlab -nodisplay -r "addpath('/projectnb/siphot/howard/git/grating_designs/aim_2021_07_16_SOPAs'); f_run_designspace_2level_aim( $1, $2, $3, '$4', '$5' ); exit"
+matlab -nodisplay -r "addpath('/projectnb/siphot/howard/git/grating_designs/aim_2021_07_16_SOPAs'); f_run_designspace_2level_aim( $1, $2, $3, '$4', '$5', $6, $7 ); exit"
